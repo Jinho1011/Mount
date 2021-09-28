e861d4e0c7c11ad7cf216f5cf1655220f6b32320
wonse <sinwonse@gmail.com>
2021-09-28 14:31:26 +0900

    Update Detail.js

--- navigation/Detail.js
+++ navigation/Detail.js
@@ -5,21 +5,13 @@ import FoodSingle from '../screens/Main/Detail/FoodSingle';
 import RecreationSet from '../screens/Main/Detail/RecreationSet';
 import RecreationSingle from '../screens/Main/Detail/RecreationSingle';
 
-const MainNavigator = createStackNavigator();
-
 const DetailNavigator = createStackNavigator();
 
-const Details = () => {
+export const Details = () => {
     <DetailNavigator.Navigator>
         <DetailNavigator.Screen name="FoodSet" component={FoodSet}/>
         <DetailNavigator.Screen name="FoodSingle" component={FoodSingle}/>
         <DetailNavigator.Screen name="RecreationSet" component={RecreationSet}/>
         <DetailNavigator.Screen name="RecreationSingle" component={RecreationSingle}/>
     </DetailNavigator.Navigator>
-};
-
-export default () => {
-    <MainNavigator.Navigator>
-        <MainNavigator.Screen name="Detail" component={Details}/>
-    </MainNavigator.Navigator>
 };
