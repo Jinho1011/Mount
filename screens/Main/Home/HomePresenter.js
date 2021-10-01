928ad2fd35fd5fba5d5c055eb0ad110608a0ae96
wonse <sinwonse@gmail.com>
2021-10-01 17:32:58 +0900

    screens/FoodSet

--- screens/Main/Home/HomePresenter.js
+++ screens/Main/Home/HomePresenter.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import { Button } from 'react-native';
 
 export default () => {
   const navigation = useNavigation();
   return (
     <View>
       <Text>Home</Text>
+      <Button 
+      title="detail"
+      onPress={() => navigation.navigate('Detail')}
+      />
     </View>
   );
 };
