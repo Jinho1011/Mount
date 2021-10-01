ef72c4119eff0de294bcf2556ad5b413bfa715cb
jinho1011 <gino9940@gmail.com>
2021-10-01 12:21:52 +0900

    Add StatusBar

---
+++ components/StatusBar.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {StatusBar} from 'react-native';
+import {useIsFocused} from '@react-navigation/native';
+
+// using in Tabs and Drawer
+// => <FocusAwareStatusBar barStyle="light-content" backgroundColor="#000000" />
+// => <FocusAwareStatusBar barStyle="dark-content" backgroundColor="#ffffff" />
+function FocusAwareStatusBar(props) {
+  const isFocused = useIsFocused();
+
+  return isFocused ? <StatusBar {...props} /> : null;
+}
