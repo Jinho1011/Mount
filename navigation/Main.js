c645a77c89939d529276dc54816181dcf51c429f
wonse <sinwonse@gmail.com>
2021-10-04 17:44:13 +0900

    detail to details
    다른 네비게이션 파일들과 파일 이름 형식을 맞추고 기존에 Details로 export했던 것과 맞추기 위해 Detail.js 파일을 Details.js로 이름을 수정했습니다

--- navigation/Main.js
+++ navigation/Main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Tabs from './Tab';
-import FoodSetPresenter from '../screens/Main/Detail/FoodSet/FoodSetPresenter';
 
 const MainNavigator = createStackNavigator();
 
@@ -12,6 +11,5 @@ export default () => (
       component={Tabs}
       options={{headerShown: false}}
     />
-    <MainNavigator.Screen name="Detail" component={FoodSetPresenter} />
   </MainNavigator.Navigator>
 );
