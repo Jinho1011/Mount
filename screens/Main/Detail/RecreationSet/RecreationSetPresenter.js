b7663a07c10e2e59d702b720dfd24e65fc51bcf9
wonse <sinwonse@gmail.com>
2021-10-08 13:55:59 +0900

    레크세트 스크린 95% 완료
    레크세트 스크린 구성 완료했습니다. 다만 마진이나 패딩등이 맞지 않는건지 피그마에 있는대로 설정을 하면 에뮬레이터에서의 화면이 같게 나오지 않네요 그래서 일단 임의로 설정한 부분들이 있습니다.

--- screens/Main/Detail/RecreationSet/RecreationSetPresenter.js
+++ screens/Main/Detail/RecreationSet/RecreationSetPresenter.js
@@ -3,6 +3,8 @@ import {View, Text, ScrollView, Image, Pressable, TouchableOpacity} from 'react-
 import {useNavigation} from '@react-navigation/native';
 import styled from 'styled-components';
 
+const PageWrap = styled.View``;
+
 const ScrollContainer = styled.ScrollView`
   background-color: #e5e5e5;
 `;
@@ -307,124 +309,341 @@ const MoreRecButtonText = styled.Text`
   color: #000000;
 `;
 
-const RecSetTotalPriceContainer = styled.View`
+const RecSetTotalPriceTitle = styled.Text`
+  margin-top: 26px;
 
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 12px;
+  line-height: 16px;
+  display: flex;
+  align-items: center;
+  color: #000000;
+`;
+
+const RecSetTotalCalculatorContainer = styled.View`
+  flex-direction: row;
+  margin-top: 12px;
+  margin-bottom: 20px;
+  padding-left: 14px;
+`;
+
+const PriceForPersonBox = styled.View`
+  flex-direction: column;
+`;
+
+const PriceForPersonTitle = styled.Text`
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 10px;
+  line-height: 15px;
+  display: flex;
+  align-items: center;
+  color: #8B8B8B;
+`;
+
+const PriceForPersonPrice = styled.Text`
+  margin-top: -5px;
+  margin-bottom: 1px;
+
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 26px;
+  display: flex;
+  align-items: center;
+  color: #8B8B8B;
+`;
+
+const CloseImageContainer = styled.View`
+  margin-top: 15px;
+  margin-left: 56px;
+  margin-bottom: 6px;
+`;
+
+const RecSetTotalCount = styled.Text`
+  margin: 9px 0 2px 14px;
+
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 26px;
+  display: flex;
+  align-items: center;
+  color: #8B8B8B;
+`;
+
+const RecSetTotalResultBox = styled.View`
+  flex-direction: column;
+  margin-left: 70px;
+`;
+
+const ResultTitle = styled.Text`
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 10px;
+  line-height: 15px;
+  display: flex;
+  align-items: center;
+  text-align: right;
+  color: #8B8B8B;
+`;
+
+const ResultPrice = styled.Text`
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 26px;
+  display: flex;
+  align-items: center;
+  text-align: right;
+  color: #FF5151;
+`;
+
+/* 주의사항 */
+const CautionContainer = styled.View`
+  margin-top: -10px;
+  padding: 24px 20px 92px 20px;
+  background: #ffffff;
+`;
+
+const CautionTitle = styled.Text`
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 12px;
+  line-height: 16px;
+  display: flex;
+  align-items: center;
+  color: #9E9E9E;
+`;
+
+const CautionDetailContainer = styled.View`
+  margin-top: 10px;
+  padding: 14px 12px 12px 12px;
+
+  background: #F3F3F3;
+`;
+
+const CautionDetail = styled.Text`
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 10px;
+  line-height: 15px;
+  display: flex;
+  align-items: center;
+  color: #8B8B8B;
+`;
+
+/* bottom button container */
+const BottomConatiner = styled.View`
+  flex-direction: row;
+  background: #FFFFFF;
+  border: 0.3px solid #B4B4B4;
+
+  padding: 8px 21px 8px 23px;
+`;
+
+const ChangeCountButton = styled.TouchableOpacity`
+  padding: 12px 118px;
+
+  background: #E2F955;
+  border-radius: 5px;
+
+  height: 48px;
+`;
+
+const ChangeCountText = styled.Text`
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: center;
+  color: #000000;
+`;
+
+const LikeButton = styled.TouchableOpacity`
+  margin: 14.5px 0 14.5px 13px;
+`;
+
+const LikeCount = styled.Text`
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 24px;
+  display: flex;
+  align-items: center;
+  color: #8B8B8B;
+
+  margin-top: 11px;
+  margin-left: 3px;
 `;
 
 export default () => {
   const navigation = useNavigation();
   return (
-    <ScrollContainer>
-      <RecImage 
-        source={require('../../../../assets/recreation_set_sample.png')}
-      />
-      <RecSetHeader>
-        <View>
-          <RecSetTitle>제목몇글자들어가나요</RecSetTitle>
-          <RecSetInfo>
-            <RecSetLabelContainer>
-              <RecSetLabel>세트</RecSetLabel>
-            </RecSetLabelContainer>
-            <RecSetElements>
-                구성품1, 구성품2, 구성품3, 구성품4, 구성품5 ...
-            </RecSetElements>
-          </RecSetInfo>
-          <RecElementsRecoContainer>
-            <RecMemberResultBlock>
-              <RecMemberResult>
-                0명이 한 팀으로 총 0팀이 만들어져요!
-              </RecMemberResult>
-            </RecMemberResultBlock>
-            <RecMemberOptionContainer>
-              <RecMembersBox>
-                <RecMembersQueHighlight />
-                <RecMembersQue>
-                  총 몇 명이{"\n"}함께 하나요?
-                </RecMembersQue>
-                <DetailAdjustMembers>
-                  <Pressable
-                    onPress={() => {
-                      console.log('minus');
-                    }}>
-                    <Image source={require('../../../../assets/minus_dis.png')} />
-                  </Pressable>
-                  <DetailMemberCounter>
-                    <CounterNumber>0</CounterNumber>
-                  </DetailMemberCounter>
-                  <Pressable
-                    onPress={() => {
-                      console.log('plus');
-                    }}>
-                    <Image source={require('../../../../assets/plus_dis.png')} />
-                  </Pressable>
-                </DetailAdjustMembers>
-              </RecMembersBox>
-              <RecTeamsBox>
-                <RecTeamsQueHighlight />
-                <RecTeamsQue>
-                  몇 팀으로{"\n"}나누고 싶나요?
-                </RecTeamsQue>
-                <DetailAdjustMembers>
-                  <Pressable
-                    onPress={() => {
-                      console.log('minus');
-                    }}>
-                    <Image source={require('../../../../assets/minus_dis.png')} />
-                  </Pressable>
-                  <DetailMemberCounter>
-                    <CounterNumber>0</CounterNumber>
-                  </DetailMemberCounter>
-                  <Pressable
-                    onPress={() => {
-                      console.log('plus');
-                    }}>
-                    <Image source={require('../../../../assets/plus_dis.png')} />
-                  </Pressable>
-                </DetailAdjustMembers>
-              </RecTeamsBox>
-            </RecMemberOptionContainer>
-          </RecElementsRecoContainer>
-        </View>       
-      </RecSetHeader>
-      <RecSetListContainer>
-        <RecSetListTitle>세트 리스트</RecSetListTitle>
-        <RecSetListItemBigContainer>
-          <RecSetListName>레크 이름 텍스트</RecSetListName>
-          <RecSetListItemTitle>구성품</RecSetListItemTitle>
-          <RecSetListItemContainer>
-            <RecSetItem>
-              <Image source={require('../../../../assets/rec_set_item_image_sample.png')} />
-              <RecSetItemName>상품이름이...</RecSetItemName>
-              <MinusPressable
-                    onPress={() => {
-                      console.log('plus');
-                    }}>
-                    <Image source={require('../../../../assets/minus.png')} />
-              </MinusPressable>
-              <RecSetItemCountBlock>
-                <RecSetItemCount>3</RecSetItemCount>
-              </RecSetItemCountBlock>
-              <PlusPressable
-                    onPress={() => {
-                      console.log('plus');
-                    }}>
-                    <Image source={require('../../../../assets/plus.png')} />
-              </PlusPressable>
-              <RecSetItemPriceContainer>
-                <RecSetItemSize>4인 (800g)</RecSetItemSize>
-                <RecSetItemPrice>16,000원</RecSetItemPrice>
-              </RecSetItemPriceContainer>          
-            </RecSetItem>
-            <RecSetBorderLine />
-            <MoreRecButton>
-              <MoreRecButtonText>레크 정보 더보기</MoreRecButtonText>
-            </MoreRecButton>
-          </RecSetListItemContainer>
-        </RecSetListItemBigContainer>
-        <RecSetTotalPriceContainer>
+    <PageWrap style={{ flex: 1 }}>
+      <ScrollContainer>
+        <RecImage 
+          source={require('../../../../assets/recreation_set_sample.png')}
+        />
+        <RecSetHeader>
+          <View>
+            <RecSetTitle>제목몇글자들어가나요</RecSetTitle>
+            <RecSetInfo>
+              <RecSetLabelContainer>
+                <RecSetLabel>세트</RecSetLabel>
+              </RecSetLabelContainer>
+              <RecSetElements>
+                  구성품1, 구성품2, 구성품3, 구성품4, 구성품5 ...
+              </RecSetElements>
+            </RecSetInfo>
+            <RecElementsRecoContainer>
+              <RecMemberResultBlock>
+                <RecMemberResult>
+                  0명이 한 팀으로 총 0팀이 만들어져요!
+                </RecMemberResult>
+              </RecMemberResultBlock>
+              <RecMemberOptionContainer>
+                <RecMembersBox>
+                  <RecMembersQueHighlight />
+                  <RecMembersQue>
+                    총 몇 명이{"\n"}함께 하나요?
+                  </RecMembersQue>
+                  <DetailAdjustMembers>
+                    <Pressable
+                      onPress={() => {
+                        console.log('minus');
+                      }}>
+                      <Image source={require('../../../../assets/minus_dis.png')} />
+                    </Pressable>
+                    <DetailMemberCounter>
+                      <CounterNumber>0</CounterNumber>
+                    </DetailMemberCounter>
+                    <Pressable
+                      onPress={() => {
+                        console.log('plus');
+                      }}>
+                      <Image source={require('../../../../assets/plus_dis.png')} />
+                    </Pressable>
+                  </DetailAdjustMembers>
+                </RecMembersBox>
+                <RecTeamsBox>
+                  <RecTeamsQueHighlight />
+                  <RecTeamsQue>
+                    몇 팀으로{"\n"}나누고 싶나요?
+                  </RecTeamsQue>
+                  <DetailAdjustMembers>
+                    <Pressable
+                      onPress={() => {
+                        console.log('minus');
+                      }}>
+                      <Image source={require('../../../../assets/minus_dis.png')} />
+                    </Pressable>
+                    <DetailMemberCounter>
+                      <CounterNumber>0</CounterNumber>
+                    </DetailMemberCounter>
+                    <Pressable
+                      onPress={() => {
+                        console.log('plus');
+                      }}>
+                      <Image source={require('../../../../assets/plus_dis.png')} />
+                    </Pressable>
+                  </DetailAdjustMembers>
+                </RecTeamsBox>
+              </RecMemberOptionContainer>
+            </RecElementsRecoContainer>
+          </View>       
+        </RecSetHeader>
+        <RecSetListContainer>
+          <RecSetListTitle>세트 리스트</RecSetListTitle>
+          <RecSetListItemBigContainer>
+            <RecSetListName>레크 이름 텍스트</RecSetListName>
+            <RecSetListItemTitle>구성품</RecSetListItemTitle>
+            <RecSetListItemContainer>
+              <RecSetItem>
+                <Image source={require('../../../../assets/rec_set_item_image_sample.png')} />
+                <RecSetItemName>상품이름이...</RecSetItemName>
+                <MinusPressable
+                      onPress={() => {
+                        console.log('plus');
+                      }}>
+                      <Image source={require('../../../../assets/minus.png')} />
+                </MinusPressable>
+                <RecSetItemCountBlock>
+                  <RecSetItemCount>3</RecSetItemCount>
+                </RecSetItemCountBlock>
+                <PlusPressable
+                      onPress={() => {
+                        console.log('plus');
+                      }}>
+                      <Image source={require('../../../../assets/plus.png')} />
+                </PlusPressable>
+                <RecSetItemPriceContainer>
+                  <RecSetItemSize>4인 (800g)</RecSetItemSize>
+                  <RecSetItemPrice>16,000원</RecSetItemPrice>
+                </RecSetItemPriceContainer>          
+              </RecSetItem>
+              <RecSetBorderLine />
+              <MoreRecButton>
+                <MoreRecButtonText>레크 정보 더보기</MoreRecButtonText>
+              </MoreRecButton>
+            </RecSetListItemContainer>
+          </RecSetListItemBigContainer>
+          <RecSetTotalPriceTitle>총 예상금액</RecSetTotalPriceTitle>        
+          <RecSetTotalCalculatorContainer>
+            <PriceForPersonBox>
+              <PriceForPersonTitle>1인 기준</PriceForPersonTitle>
+              <PriceForPersonPrice>12000원</PriceForPersonPrice>
+            </PriceForPersonBox>
+            <CloseImageContainer>
+              <Image source={require('../../../../assets/close.png')} /> 
+            </CloseImageContainer>
+            <RecSetTotalCount>20</RecSetTotalCount>  
+            <RecSetTotalResultBox>
+              <ResultTitle>총 금액</ResultTitle>
+              <ResultPrice>999,999원</ResultPrice>
+            </RecSetTotalResultBox>          
+          </RecSetTotalCalculatorContainer>    
+        </RecSetListContainer>
+        <RecSetBorderLine />
+        <CautionContainer>
+          <CautionTitle>주의 사항</CautionTitle>
+          <CautionDetailContainer>
+            <CautionDetail>
+            미인을 옷을 불어 산야에 사라지지 굳세게 얼마나 열락의 교향악이다.
+            그러므로 얼음과 지혜는 같은 내는 바이며, 그들에게 수 끓는다. 위하여,
+            노년에게서 얼마나 쓸쓸한 황금시대의 기쁘며, 같이, 아름다우냐?{"\n"}{"\n"}
 
-        </RecSetTotalPriceContainer>      
-      </RecSetListContainer>
-    </ScrollContainer>
+            않는 피어나는 이것을 인간은 인생에 청춘을 우리 못할 무엇을 있다.
+            목숨이 있는 같이, 그러므로 것이다. 장식하는 오직 그들에게 하였으며,
+            품에 인생{"\n"}{"\n"}
+
+            미인을 옷을 불어 산야에 사라지지 굳세게 얼마나 열락의 교향악이다. 
+            그러므로 얼음과 지혜는 같은 내는 바이며, 그들에게 수 끓는다. 
+            위하여, 노년에게서 얼마나 쓸쓸한 황금시대의 기쁘며, 같이, 아름다우냐? 
+            </CautionDetail>
+          </CautionDetailContainer>
+        </CautionContainer>
+      </ScrollContainer>
+      <BottomConatiner>
+        <ChangeCountButton>
+          <ChangeCountText>수량변경</ChangeCountText>
+        </ChangeCountButton>
+        <LikeButton>
+          <Image source={require('../../../../assets/Like.png')} /> 
+        </LikeButton>
+        <LikeCount>23</LikeCount>
+      </BottomConatiner>
+    </PageWrap>
   );
 };
