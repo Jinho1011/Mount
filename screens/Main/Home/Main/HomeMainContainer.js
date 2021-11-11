053eb8ad8c8f0869acf419bf300298cc4db54051
jinho1011 <gino9940@gmail.com>
2021-11-12 00:20:26 +0900

    Fetch /home/foods data

--- screens/Main/Home/Main/HomeMainContainer.js
+++ screens/Main/Home/Main/HomeMainContainer.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import HomePresenter from './HomeMainPresenter';
 
 export default () => {
-  return <HomePresenter />;
+  const [foods, setFoods] = useState([]);
+
+  useEffect(async () => {
+    let data = await fetch('/api/home/foods');
+    let foods = JSON.parse(data._bodyInit).foods;
+    setFoods(foods);
+  }, []);
+
+  return <HomePresenter foods={foods} />;
 };
