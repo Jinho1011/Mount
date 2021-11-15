c223109f8b98f2c3663de9568256f04e139c3aa2
jinho1011 <gino9940@gmail.com>
2021-11-15 22:39:47 +0900

    Add States

--- screens/Main/Detail/FoodSet/FoodSetContainer.js
+++ screens/Main/Detail/FoodSet/FoodSetContainer.js
@@ -2,46 +2,58 @@ import React, {useEffect, useState} from 'react';
 import FoodSetPresenter from './FoodSetPresenter';
 
 const FoodSetContainer = () => {
-  const [membercount, setMembercount] = useState(0);
-  const [itemscount, setItemscount] = useState(0);
-  const [foodSet, setFoodSet] = useState([]);
-  const [items, setItems] = useState([]);
+  const [state, setState] = useState({
+    memberCnt: 0,
+    itemCnt: 0,
+    foodSet: [],
+    items: [],
+    isLoaded: false,
+  });
+  // const [membercount, setMembercount] = useState(0);
+  // const [itemscount, setItemscount] = useState(0);
+  // const [foodSet, setFoodSet] = useState([]);
+  // const [items, setItems] = useState([]);
 
-  useEffect(async () => {
-    let data = await fetch('/api/detail/foodSet');
-    let foodSet = JSON.parse(data._bodyInit).foodSet;
-    let items = JSON.parse(data._bodyInit).items;
-    setFoodSet(foodSet);
-    setItems(items);
+  useEffect(() => {
+    const init = async () => {
+      let data = await fetch('/api/detail/foodSet');
+      let foodSet = JSON.parse(data._bodyInit).foodSet;
+      let items = JSON.parse(data._bodyInit).items;
+
+      setState(prev => ({
+        ...prev, // -> memCnt, itemCnt, isLoaded는 변하지 않음 = 이전 데이터랑 같음
+        foodSet, // 변화
+        items, // 변화
+        isLoaded: true,
+      }));
+    };
+    init();
   }, []);
 
-  const onIncreaseMember = () => {
-    setMembercount(membercount + 1);
-  };
+  // const onIncreaseMember = () => {
+  //   setMembercount(membercount + 1);
+  // };
 
-  const onDecreaseMember = () => {
-    setMembercount(Math.max(0, membercount - 1)); // 인원수는 음수가 되면 안됨
-  };
+  // const onDecreaseMember = () => {
+  //   setMembercount(Math.max(0, membercount - 1)); // 인원수는 음수가 되면 안됨
+  // };
 
-  const onIncreaseItems = () => {
-    setItemscount(itemscount + 1);
-  };
+  // const onIncreaseItems = () => {
+  //   setItemscount(itemscount + 1);
+  // };
 
-  const onDecreaseItems = () => {
-    setItemscount(Math.max(0, itemscount - 1));
-  };
+  // const onDecreaseItems = () => {
+  //   setItemscount(Math.max(0, itemscount - 1));
+  // };
 
   return (
-    <FoodSetPresenter
-      foodSet={foodSet}
-      membercount={membercount}
-      items={items}
-      itemscount={itemscount}
-      onIncreaseMember={onIncreaseMember}
-      onDecreaseMember={onDecreaseMember}
-      onIncreaseItems={onIncreaseItems}
-      onDecreaseItems={onDecreaseItems}
-    />
+    <>
+      {isLoaded ? (
+        <FoodSetPresenter state={state} setState={setState} />
+      ) : (
+        <View>Loading Page</View>
+      )}
+    </>
   );
 };
 
