c7fd84f01d4dc399fd0ccd3545b3707ecb99c436
jinho1011 <gino9940@gmail.com>
2021-11-15 16:11:59 +0900

    Add dark StatusBar

--- screens/Main/Detail/FoodSet/FoodSetPresenter.js
+++ screens/Main/Detail/FoodSet/FoodSetPresenter.js
@@ -11,8 +11,9 @@ import {
 import {useNavigation} from '@react-navigation/native';
 import styled from 'styled-components';
 
-const PageWrap = styled.View`
-`;
+import FocusAwareStatusBar from '../../../../components/StatusBar';
+
+const PageWrap = styled.View``;
 
 const ScrollContainer = styled.ScrollView`
   background-color: #e5e5e5;
@@ -155,7 +156,7 @@ const PlusPressable = styled.Pressable`
 `;
 
 const FoodSetItemCountBlock = styled.View`
-  background: #F3F3F3;
+  background: #f3f3f3;
   border-radius: 12.5px;
   margin-top: 3px;
   margin-left: 6px;
@@ -198,7 +199,7 @@ const FoodSetItemPrice = styled.Text`
 `;
 
 const FoodSetBorderLine = styled.View`
-  border: 0.35px solid #EAEAEA;
+  border: 0.35px solid #eaeaea;
 
   margin-top: 14px;
 `;
@@ -206,7 +207,7 @@ const FoodSetBorderLine = styled.View`
 const MoreRecButton = styled.TouchableOpacity`
   padding: 12px 76px;
   margin: 24px 28px 22px 28px;
-  background: #E2F955;
+  background: #e2f955;
   border-radius: 5px;
 `;
 
@@ -252,7 +253,7 @@ const PriceForPersonTitle = styled.Text`
   line-height: 15px;
   display: flex;
   align-items: center;
-  color: #8B8B8B;
+  color: #8b8b8b;
 `;
 
 const PriceForPersonPrice = styled.Text`
@@ -266,7 +267,7 @@ const PriceForPersonPrice = styled.Text`
   line-height: 26px;
   display: flex;
   align-items: center;
-  color: #8B8B8B;
+  color: #8b8b8b;
 `;
 
 const CloseImageContainer = styled.View`
@@ -285,7 +286,7 @@ const FoodSetTotalCount = styled.Text`
   line-height: 26px;
   display: flex;
   align-items: center;
-  color: #8B8B8B;
+  color: #8b8b8b;
 `;
 
 const FoodSetTotalResultBox = styled.View`
@@ -302,7 +303,7 @@ const ResultTitle = styled.Text`
   display: flex;
   align-items: center;
   text-align: right;
-  color: #8B8B8B;
+  color: #8b8b8b;
 `;
 
 const ResultPrice = styled.Text`
@@ -314,7 +315,7 @@ const ResultPrice = styled.Text`
   display: flex;
   align-items: center;
   text-align: right;
-  color: #FF5151;
+  color: #ff5151;
 `;
 
 /* 주의사항 */
@@ -332,14 +333,14 @@ const CautionTitle = styled.Text`
   line-height: 16px;
   display: flex;
   align-items: center;
-  color: #9E9E9E;
+  color: #9e9e9e;
 `;
 
 const CautionDetailContainer = styled.View`
   margin-top: 10px;
   padding: 14px 12px 12px 12px;
 
-  background: #F3F3F3;
+  background: #f3f3f3;
 `;
 
 const CautionDetail = styled.Text`
@@ -350,14 +351,14 @@ const CautionDetail = styled.Text`
   line-height: 15px;
   display: flex;
   align-items: center;
-  color: #8B8B8B;
+  color: #8b8b8b;
 `;
 
 /* bottom button container */
 const BottomConatiner = styled.View`
   flex-direction: row;
-  background: #FFFFFF;
-  border: 0.3px solid #B4B4B4;
+  background: #ffffff;
+  border: 0.3px solid #b4b4b4;
 
   padding: 8px 21px 8px 23px;
 `;
@@ -365,7 +366,7 @@ const BottomConatiner = styled.View`
 const ChangeCountButton = styled.TouchableOpacity`
   padding: 12px 118px;
 
-  background: #E2F955;
+  background: #e2f955;
   border-radius: 5px;
 
   height: 48px;
@@ -393,7 +394,7 @@ const LikeCount = styled.Text`
   line-height: 24px;
   display: flex;
   align-items: center;
-  color: #8B8B8B;
+  color: #8b8b8b;
 
   margin-top: 11px;
   margin-left: 3px;
@@ -402,7 +403,9 @@ const LikeCount = styled.Text`
 export default () => {
   const navigation = useNavigation();
   return (
-    <PageWrap style={{ flex: 1 }}>
+    <PageWrap style={{flex: 1}}>
+      <FocusAwareStatusBar barStyle="light-content" backgroundColor="#000000" />
+
       <ScrollContainer>
         <FoodImage
           source={require('../../../../assets/food_detail_sample.png')}
@@ -441,80 +444,84 @@ export default () => {
           </DetailMembers>
         </DetailHeader>
         <FoodSetListContainer>
-            <FoodSetListTitle>구성품</FoodSetListTitle>
-            <FoodSetListItemBigContainer>
-              <FoodSetListItemContainer>
-                <FoodSetItem>
-                  <Image source={require('../../../../assets/rec_set_item_image_sample.png')} />
-                  <FoodSetItemName>상품이름이...</FoodSetItemName>
-                  <MinusPressable
-                        onPress={() => {
-                          console.log('plus');
-                        }}>
-                        <Image source={require('../../../../assets/minus.png')} />
-                  </MinusPressable>
-                  <FoodSetItemCountBlock>
-                    <FoodSetItemCount>3</FoodSetItemCount>
-                  </FoodSetItemCountBlock>
-                  <PlusPressable
-                        onPress={() => {
-                          console.log('plus');
-                        }}>
-                        <Image source={require('../../../../assets/plus.png')} />
-                  </PlusPressable>
-                  <FoodSetItemPriceContainer>
-                    <FoodSetItemSize>4인 (800g)</FoodSetItemSize>
-                    <FoodSetItemPrice>16,000원</FoodSetItemPrice>
-                  </FoodSetItemPriceContainer>          
-                </FoodSetItem>
-                <FoodSetBorderLine />
-              </FoodSetListItemContainer>
-            </FoodSetListItemBigContainer>
-            <FoodSetTotalPriceTitle>총 예상금액</FoodSetTotalPriceTitle>        
-            <FoodSetTotalCalculatorContainer>
-              <PriceForPersonBox>
-                <PriceForPersonTitle>1인 기준</PriceForPersonTitle>
-                <PriceForPersonPrice>12000원</PriceForPersonPrice>
-              </PriceForPersonBox>
-              <CloseImageContainer>
-                <Image source={require('../../../../assets/close.png')} /> 
-              </CloseImageContainer>
-              <FoodSetTotalCount>20</FoodSetTotalCount>  
-              <FoodSetTotalResultBox>
-                <ResultTitle>총 금액</ResultTitle>
-                <ResultPrice>999,999원</ResultPrice>
-              </FoodSetTotalResultBox>          
-            </FoodSetTotalCalculatorContainer>    
-          </FoodSetListContainer>
-          <FoodSetBorderLine />
-          <CautionContainer>
-            <CautionTitle>주의 사항</CautionTitle>
-            <CautionDetailContainer>
-              <CautionDetail>
+          <FoodSetListTitle>구성품</FoodSetListTitle>
+          <FoodSetListItemBigContainer>
+            <FoodSetListItemContainer>
+              <FoodSetItem>
+                <Image
+                  source={require('../../../../assets/rec_set_item_image_sample.png')}
+                />
+                <FoodSetItemName>상품이름이...</FoodSetItemName>
+                <MinusPressable
+                  onPress={() => {
+                    console.log('plus');
+                  }}>
+                  <Image source={require('../../../../assets/minus.png')} />
+                </MinusPressable>
+                <FoodSetItemCountBlock>
+                  <FoodSetItemCount>3</FoodSetItemCount>
+                </FoodSetItemCountBlock>
+                <PlusPressable
+                  onPress={() => {
+                    console.log('plus');
+                  }}>
+                  <Image source={require('../../../../assets/plus.png')} />
+                </PlusPressable>
+                <FoodSetItemPriceContainer>
+                  <FoodSetItemSize>4인 (800g)</FoodSetItemSize>
+                  <FoodSetItemPrice>16,000원</FoodSetItemPrice>
+                </FoodSetItemPriceContainer>
+              </FoodSetItem>
+              <FoodSetBorderLine />
+            </FoodSetListItemContainer>
+          </FoodSetListItemBigContainer>
+          <FoodSetTotalPriceTitle>총 예상금액</FoodSetTotalPriceTitle>
+          <FoodSetTotalCalculatorContainer>
+            <PriceForPersonBox>
+              <PriceForPersonTitle>1인 기준</PriceForPersonTitle>
+              <PriceForPersonPrice>12000원</PriceForPersonPrice>
+            </PriceForPersonBox>
+            <CloseImageContainer>
+              <Image source={require('../../../../assets/close.png')} />
+            </CloseImageContainer>
+            <FoodSetTotalCount>20</FoodSetTotalCount>
+            <FoodSetTotalResultBox>
+              <ResultTitle>총 금액</ResultTitle>
+              <ResultPrice>999,999원</ResultPrice>
+            </FoodSetTotalResultBox>
+          </FoodSetTotalCalculatorContainer>
+        </FoodSetListContainer>
+        <FoodSetBorderLine />
+        <CautionContainer>
+          <CautionTitle>주의 사항</CautionTitle>
+          <CautionDetailContainer>
+            <CautionDetail>
               미인을 옷을 불어 산야에 사라지지 굳세게 얼마나 열락의 교향악이다.
-              그러므로 얼음과 지혜는 같은 내는 바이며, 그들에게 수 끓는다. 위하여,
-              노년에게서 얼마나 쓸쓸한 황금시대의 기쁘며, 같이, 아름다우냐?{"\n"}{"\n"}
-
+              그러므로 얼음과 지혜는 같은 내는 바이며, 그들에게 수 끓는다.
+              위하여, 노년에게서 얼마나 쓸쓸한 황금시대의 기쁘며, 같이,
+              아름다우냐?{'\n'}
+              {'\n'}
               않는 피어나는 이것을 인간은 인생에 청춘을 우리 못할 무엇을 있다.
-              목숨이 있는 같이, 그러므로 것이다. 장식하는 오직 그들에게 하였으며,
-              품에 인생{"\n"}{"\n"}
-
-              미인을 옷을 불어 산야에 사라지지 굳세게 얼마나 열락의 교향악이다. 
-              그러므로 얼음과 지혜는 같은 내는 바이며, 그들에게 수 끓는다. 
-              위하여, 노년에게서 얼마나 쓸쓸한 황금시대의 기쁘며, 같이, 아름다우냐? 
-              </CautionDetail>
-            </CautionDetailContainer>
-          </CautionContainer>
-        </ScrollContainer>
-        <BottomConatiner>
-          <ChangeCountButton>
-            <ChangeCountText>수량변경</ChangeCountText>
-          </ChangeCountButton>
-          <LikeButton>
-            <Image source={require('../../../../assets/Like.png')} /> 
-          </LikeButton>
-          <LikeCount>23</LikeCount>
-        </BottomConatiner>
+              목숨이 있는 같이, 그러므로 것이다. 장식하는 오직 그들에게
+              하였으며, 품에 인생{'\n'}
+              {'\n'}
+              미인을 옷을 불어 산야에 사라지지 굳세게 얼마나 열락의 교향악이다.
+              그러므로 얼음과 지혜는 같은 내는 바이며, 그들에게 수 끓는다.
+              위하여, 노년에게서 얼마나 쓸쓸한 황금시대의 기쁘며, 같이,
+              아름다우냐?
+            </CautionDetail>
+          </CautionDetailContainer>
+        </CautionContainer>
+      </ScrollContainer>
+      <BottomConatiner>
+        <ChangeCountButton>
+          <ChangeCountText>수량변경</ChangeCountText>
+        </ChangeCountButton>
+        <LikeButton>
+          <Image source={require('../../../../assets/Like.png')} />
+        </LikeButton>
+        <LikeCount>23</LikeCount>
+      </BottomConatiner>
     </PageWrap>
   );
 };
