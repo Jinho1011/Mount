98922dd66003de102ad397a7de91df28851fa526
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2021-11-22 22:32:56 +0900

    음식 세트/싱글 컴포넌트화

--- screens/Main/Detail/FoodSingle/FoodSingleContainer.js
+++ screens/Main/Detail/FoodSingle/FoodSingleContainer.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import FoodSinglePresenter from './FoodSinglePresenter';
 
-export default () => {
-  return <FoodSinglePresenter />;
-};
+const FoodSingleContainer = () => {
+  const [state, setState] = useState({
+    memberCnt: 0,
+    foodSingle: [],
+  });
+
+  useEffect(() => {
+    const init = async () => {
+      let data = await fetch('/api/detail/foodSingle');
+      let foodSingle = JSON.parse(data._bodyInit).foodSingle;
+      console.log(foodSingle);
+      setState(prev => ({
+        ...prev,
+        foodSingle,
+      }));
+    };
+    init();
+  },[]);
+
+  return (
+    <>
+      <FoodSinglePresenter state={state} setState={setState} />
+    </>
+  )
+}
+
+export default FoodSingleContainer;
