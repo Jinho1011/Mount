502a657a685c01309d41c8dbb3dc0cff4c5b9842
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2021-11-22 22:33:46 +0900

    추가된 mock data

---
+++ data/detail/foodSingle.js
@@ -0,0 +1,14 @@
+export default () => {
+    return {
+      foodSingle: [
+        {
+          id: 1,
+          img: 'https://dummyimage.com/600x253/000/fff',
+          title: '고기',
+          info: '1인분 200g 기준',
+          price: 12000,
+        },
+      ],
+    };
+  };
+  
