1efdf32035c58d82f25ab788179a7c5cfc7b0999
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2021-11-22 22:34:02 +0900

    라우트

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import {createServer} from 'miragejs';
 
 import homeFoods from './data/home/foods';
 import foodSet from './data/detail/foodSet';
+import foodSingle from './data/detail/foodSingle';
+import recSet from './data/detail/recSet';
 
 if (window.server) {
   server.shutdown();
@@ -13,6 +15,8 @@ window.server = createServer({
   routes() {
     this.get('/api/home/foods', homeFoods);
     this.get('/api/detail/foodSet', foodSet);
+    this.get('/api/detail/foodSingle', foodSingle);
+    this.get('/api/detail/recSet', recSet);
   },
 });
 
