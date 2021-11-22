4dd24a37ead85cc328b5d7d4fd3dc7bc49d9d046
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2021-11-22 22:33:57 +0900

    레크 세트 컴포넌트화

--- screens/Main/Detail/RecreationSet/RecreationSetPresenter.js
+++ screens/Main/Detail/RecreationSet/RecreationSetPresenter.js
@@ -2,169 +2,14 @@ import React from 'react';
 import {View, Text, ScrollView, Image, Pressable, TouchableOpacity} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import styled from 'styled-components';
+import TitleContainer from '../../../../components/Common/SetTitle';
+import RecreationSetContainer from './RecreationSetContainer';
+import Counter from '../../../../components/Rec/Counter';
 
 const PageWrap = styled.View``;
 
 const ScrollContainer = styled.ScrollView`
   background-color: #e5e5e5;
-`;
-
-const RecImage = styled.Image`
-  width: 100%;
-  height: 254px;
-`;
-
-const RecSetHeader = styled.View`
-  padding: 16px 20px 20px 20px;
-  justify-content: space-between;
-  background-color: #fff;
-`;
-
-const RecSetTitle = styled.Text`
-  font-family: Noto Sans KR;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 18px;
-  line-height: 26px;
-  color: #000000;
-`;
-
-const RecSetInfo = styled.View`
-  flex-direction: row;
-`;
-
-const RecSetLabelContainer = styled.View`
-  padding: 2px 4px 1px 4px;
-  background-color: #F3F3F3;
-  border-radius: 6px;
-  height: 16px;
-  justify-content: center;
-  align-items: center;
-  margin-right: 4px;
-`;
-
-const RecSetLabel = styled.Text`
-  font-family: Noto Sans KR;
-  font-size: 8px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 12px;
-  text-align: left;
-`;
-
-const RecSetElements = styled.Text`
-  font-family: Noto Sans KR;
-  font-size: 10px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 15px;
-  text-align: left;
-  color: #777777;
-`;
-
-const RecElementsRecoContainer = styled.View`
-  width: 100%;
-  height: 172px;
-  margin-top: 16px;
-  justify-content: space-between;
-`;
-
-const RecMemberResultBlock = styled.View`
-  height: 52px;
-  border-radius: 8px;
-  background-color: #E2F955;
-`;
-
-const RecMemberResult = styled.Text`
-  font-family: Noto Sans KR;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 24px;
-  margin-left: 22px;
-  margin-top: 14px;
-  color: #000000;
-`;
-
-const RecMemberOptionContainer = styled.View`
-  flex-direction: row;
-  margin-top: 10px;
-  margin-right: 10px;
-  height: 110px;
-`;
-
-const RecMembersBox = styled.View`
-  width: 50%;
-  margin-right: 10px;
-  border: 1px solid #EAEAEA;
-  border-radius: 8px;
-`;
-
-const RecTeamsBox = styled.View`
-  width: 50%;
-  border: 1px solid #EAEAEA;
-  border-radius: 8px; 
-`;
-
-const RecMembersQue = styled.Text`
-  font-family: Noto Sans KR;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 14px;
-  line-height: 20px;
-  color: #000000;
-
-  margin-left: 12px;
-  margin-top: -21px;
-`;
-
-const RecMembersQueHighlight = styled.View`
-  background: #E2F955;
-  width: 48px;
-  height: 10px;
-  margin-left: 10px;
-  margin-top: 24px;
-`;
-
-const RecTeamsQue = styled.Text`
-  font-family: Noto Sans KR;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 14px;
-  line-height: 20px;
-  color: #000000;
-
-  margin-left: 12px;
-  margin-top: -21px;
-`;
-
-const RecTeamsQueHighlight = styled.View`
-  background: #E2F955;
-  width: 48px;
-  height: 10px;
-  margin-left: 10px;
-  margin-top: 24px;
-`;
-
-const DetailAdjustMembers = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  margin: 18px 12px 16px 12px;
-`;
-
-const DetailMemberCounter = styled.View`
-  margin: 0 14px;
-  padding: 0 22px;
-  border-radius: 6px;
-  border: 1px solid #f3f3f3;
-`;
-
-const CounterNumber = styled.Text`
-  color: #000;
-  font-size: 16px;
-  line-height: 24px;
-  font-family: 'NotoSansKR-Bold';
 `;
 
 /* 레크레이션 세트 리스트 */
@@ -488,82 +333,16 @@ const LikeCount = styled.Text`
   margin-left: 3px;
 `;
 
-export default () => {
-  const navigation = useNavigation();
+const RecreationSetPresenter = ({state, setState}) => {
   return (
     <PageWrap style={{ flex: 1 }}>
       <ScrollContainer>
-        <RecImage 
-          source={require('../../../../assets/recreation_set_sample.png')}
+        <TitleContainer
+          img={state?.recSet[0]?.img}
+          title={state?.recSet[0]?.title}
+          components={state?.recSet[0]?.components}
         />
-        <RecSetHeader>
-          <View>
-            <RecSetTitle>제목몇글자들어가나요</RecSetTitle>
-            <RecSetInfo>
-              <RecSetLabelContainer>
-                <RecSetLabel>세트</RecSetLabel>
-              </RecSetLabelContainer>
-              <RecSetElements>
-                  구성품1, 구성품2, 구성품3, 구성품4, 구성품5 ...
-              </RecSetElements>
-            </RecSetInfo>
-            <RecElementsRecoContainer>
-              <RecMemberResultBlock>
-                <RecMemberResult>
-                  0명이 한 팀으로 총 0팀이 만들어져요!
-                </RecMemberResult>
-              </RecMemberResultBlock>
-              <RecMemberOptionContainer>
-                <RecMembersBox>
-                  <RecMembersQueHighlight />
-                  <RecMembersQue>
-                    총 몇 명이{"\n"}함께 하나요?
-                  </RecMembersQue>
-                  <DetailAdjustMembers>
-                    <Pressable
-                      onPress={() => {
-                        console.log('minus');
-                      }}>
-                      <Image source={require('../../../../assets/minus_dis.png')} />
-                    </Pressable>
-                    <DetailMemberCounter>
-                      <CounterNumber>0</CounterNumber>
-                    </DetailMemberCounter>
-                    <Pressable
-                      onPress={() => {
-                        console.log('plus');
-                      }}>
-                      <Image source={require('../../../../assets/plus_dis.png')} />
-                    </Pressable>
-                  </DetailAdjustMembers>
-                </RecMembersBox>
-                <RecTeamsBox>
-                  <RecTeamsQueHighlight />
-                  <RecTeamsQue>
-                    몇 팀으로{"\n"}나누고 싶나요?
-                  </RecTeamsQue>
-                  <DetailAdjustMembers>
-                    <Pressable
-                      onPress={() => {
-                        console.log('minus');
-                      }}>
-                      <Image source={require('../../../../assets/minus_dis.png')} />
-                    </Pressable>
-                    <DetailMemberCounter>
-                      <CounterNumber>0</CounterNumber>
-                    </DetailMemberCounter>
-                    <Pressable
-                      onPress={() => {
-                        console.log('plus');
-                      }}>
-                      <Image source={require('../../../../assets/plus_dis.png')} />
-                    </Pressable>
-                  </DetailAdjustMembers>
-                </RecTeamsBox>
-              </RecMemberOptionContainer>
-            </RecElementsRecoContainer>
-          </View>       
-        </RecSetHeader>
+        <Counter state={state} setState={setState} />
         <RecSetListContainer>
           <RecSetListTitle>세트 리스트</RecSetListTitle>
           <RecSetListItemBigContainer>
@@ -647,3 +426,5 @@ export default () => {
     </PageWrap>
   );
 };
+
+export default RecreationSetPresenter;
