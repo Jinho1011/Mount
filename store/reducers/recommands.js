9a18aab5317802723d2a33a5bfe65dbbc05521ad
jinho1011 <gino9940@gmail.com>
2021-11-28 16:33:59 +0900

    Add actions

--- store/reducers/recommands.js
+++ store/reducers/recommands.js
@@ -1,3 +1,25 @@
-export default (state = [], actions) => {
-  return state;
+const initialState = {
+  foods: [],
+  recs: [],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'INIT_FOODS': {
+      const FOODS = action.foods;
+      return {
+        ...state,
+        foods: FOODS,
+      };
+    }
+    case 'INIT_RECS': {
+      const RECS = action.recs;
+      return {
+        ...state,
+        recs: RECS,
+      };
+    }
+    default:
+      return state;
+  }
 };
