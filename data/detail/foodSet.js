f2e59d7f84d67d0d6052f5a828c52388f5aa20c9
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2021-11-30 00:10:22 +0900

    Update foodSet.js
    음식 세트 mock data 형식 변경입니다

--- data/detail/foodSet.js
+++ data/detail/foodSet.js
@@ -1,29 +1,26 @@
-export default () => {
-  return {
-    foodSet: [
-      {
-        id: 1,
-        img: 'https://dummyimage.com/600x253/000/fff',
-        title: '제목몇글자들어가나요',
-        components: '구성품1, 구성품2, 구성품3, 구성품4',
-      },
-    ],
+const foodSet = [
+  {
+    id: 1,
+    img: 'https://dummyimage.com/600x253/000/fff',
+    title: '제목몇글자들어가나요',
     items: [
       {
         id: 1,
-        name: '가나다라마바사',
+        name: '고기',
         price: 16000,
       },
       {
         id: 2,
-        name: '가나다라마바사',
+        name: '밥',
         price: 17000,
       },
       {
         id: 3,
-        name: '가나다라마바사',
+        name: '야채',
         price: 18000,
       },
     ],
-  };
-};
+  }
+]
+
+export default foodSet;
