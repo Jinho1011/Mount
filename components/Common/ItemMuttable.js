0c3f9d76e2ef13a68b99c7c586cf7dcee4fd882a
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2021-11-30 00:09:51 +0900

    Create ItemMuttable.js
    오늘 회의에서 디자인팀에서 전달해준 +, - 가 가능한 Item 컴포넌트입니다

---
+++ components/Common/ItemMuttable.js
@@ -0,0 +1,148 @@
+/**
+ * 플러스, 마이너스가 가능한 구성품 컴포넌트입니다.
+ */
+ import {isEmptyStatement} from '@babel/types';
+ import React from 'react';
+ import {Image, Pressable} from 'react-native';
+ import styled from 'styled-components';
+ 
+ // 구성품 + 구분선
+ const ItemBox = styled.View`
+   margin-top: 14px;
+   flex: 1;
+   width: 100%;
+   height: 46px;
+ `;
+ 
+ // 구성품
+ const ItemSmallBox = styled.View`
+   flex-direction: row;
+   margin-bottom: 14px;
+   height: 32px;
+ `;
+ 
+ const ItemName = styled.Text`
+   font-family: 'NotoSansKR-Regular';
+   font-style: normal;
+   font-weight: normal;
+   font-size: 14px;
+   line-height: 20px;
+   color: #000000;
+ 
+   margin-left: 10px;
+   margin-top: 5px;
+ `;
+ 
+ const MinusPressable = styled.Pressable`
+   padding: 16px 0 16px 24px;
+ `;
+ 
+ const PlusPressable = styled.Pressable`
+   padding: 11px 0 11px 6px;
+ `;
+ 
+ const ItemCountBlock = styled.View`
+   background: #f3f3f3;
+   border-radius: 12.5px;
+   height: 25px;
+   margin-top: 3px;
+   margin-left: 6px;
+   padding: 2px 16px 3px 16px;
+ `;
+ 
+ const ItemCount = styled.Text`
+   color: #000000;
+   font-size: 14px;
+   font-family: 'NotoSansKR-Regular';
+   line-height: 20px;
+ `;
+ 
+ const SetItemPriceContainer = styled.View`
+   flex-direction: column;
+   align-items: flex-end;
+   margin-left: 12px;
+ `;
+ 
+ const SetItemize = styled.Text`
+   font-family: 'NotoSansKR-Normal';
+   font-size: 10px;
+   line-height: 15px;
+   text-align: right;
+   color: #828282;
+ `;
+ 
+ const SetItemPrice = styled.Text`
+   font-family: 'NotoSansKR-Bold';
+   font-size: 14px;
+   line-height: 20px;
+   text-align: right;
+ `;
+ 
+ const SetBorderLine = styled.View`
+   border: 0.35px solid #eaeaea;
+ `;
+ 
+ const Item = ({state, setState, item}) => {
+   const minusCount = () => {
+     let items = state.items.map(_item => {
+       if (item.id === _item.id) {
+         return {
+           ..._item,
+           count: _item.count - 1,
+         };
+       } else {
+         return _item;
+       }
+     });
+ 
+     setState(prev => ({
+       ...prev,
+       items,
+     }));
+   };
+ 
+ 
+   const plusCount = () => {
+     let items = state.items.map(_item => {
+       if (item.id === _item.id) {
+         return {
+           ..._item,
+           count: _item.count + 1,
+         };
+       } else {
+         return _item;
+       }
+     });
+ 
+     setState(prev => ({
+       ...prev,
+       items,
+     }));
+   };
+ 
+   return (
+     <ItemBox>
+       <ItemSmallBox>
+         <Image source={require('../../assets/rec_set_item_image_sample.png')} />
+         <ItemName>{item.name}</ItemName>
+         <MinusPressable onPress={minusCount}>
+           <Image source={require('../../assets/minus.png')} />
+         </MinusPressable>
+         <ItemCountBlock>
+           <ItemCount>0</ItemCount>
+         </ItemCountBlock>
+         <PlusPressable onPress={plusCount}>
+           <Image source={require('../../assets/plus.png')} />
+         </PlusPressable>
+         <SetItemPriceContainer>
+           <SetItemize>4인 (800g)</SetItemize>
+           <SetItemPrice>{item.price}원</SetItemPrice>
+         </SetItemPriceContainer>
+       </ItemSmallBox>
+       <SetBorderLine />
+     </ItemBox>
+   );
+ };
+ 
+ export default Item;
+ 
