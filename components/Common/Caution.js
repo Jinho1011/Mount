de58121c8a3285f9fc42665c4015e9d3d89fb52a
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2021-11-30 16:17:47 +0900

    Caution 컴포넌트 수정
    caution을 받아옵니다

--- components/Common/Caution.js
+++ components/Common/Caution.js
@@ -37,25 +37,13 @@ const CautionDetail = styled.Text`
   color: #8b8b8b;
 `;
 
-const Caution = () => {
+const Caution = ({state, setState, caution}) => {
     return (
         <CautionContainer>
           <CautionTitle>주의 사항</CautionTitle>
           <CautionDetailContainer>
             <CautionDetail>
-              미인을 옷을 불어 산야에 사라지지 굳세게 얼마나 열락의 교향악이다.
-              그러므로 얼음과 지혜는 같은 내는 바이며, 그들에게 수 끓는다.
-              위하여, 노년에게서 얼마나 쓸쓸한 황금시대의 기쁘며, 같이,
-              아름다우냐?{'\n'}
-              {'\n'}
-              않는 피어나는 이것을 인간은 인생에 청춘을 우리 못할 무엇을 있다.
-              목숨이 있는 같이, 그러므로 것이다. 장식하는 오직 그들에게
-              하였으며, 품에 인생{'\n'}
-              {'\n'}
-              미인을 옷을 불어 산야에 사라지지 굳세게 얼마나 열락의 교향악이다.
-              그러므로 얼음과 지혜는 같은 내는 바이며, 그들에게 수 끓는다.
-              위하여, 노년에게서 얼마나 쓸쓸한 황금시대의 기쁘며, 같이,
-              아름다우냐?
+              {caution}
             </CautionDetail>
           </CautionDetailContainer>
         </CautionContainer>
