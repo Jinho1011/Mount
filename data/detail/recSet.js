81fd27639bf502454f6a2299f65ba4b6397158ef
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2021-11-30 16:49:26 +0900

    레크 세트 수정

--- data/detail/recSet.js
+++ data/detail/recSet.js
@@ -3,7 +3,6 @@ const recSet = [
     id: 1,
     img: 'https://dummyimage.com/600x253/000/fff',
     title: '제목몇글자들어가나요',
-    components: '구성품1, 구성품2, 구성품3, 구성품4',
     items: [
       {
         id: 1,
