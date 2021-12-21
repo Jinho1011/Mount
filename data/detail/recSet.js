64fa370bac512f03b25b71b7aa75094913b2d518
jinho1011 <gino9940@gmail.com>
2021-12-21 20:19:32 +0900

    Add subtitle in datas

--- data/detail/recSet.js
+++ data/detail/recSet.js
@@ -3,7 +3,7 @@ export default [
     id: 1,
     img: 'https://dummyimage.com/600x253/000/fff',
     title: '제목몇글자들어가나요',
-    subtitle: '',
+    subtitle: '세트설명어쩌구저쩌구어쩌..',
     caution: '주의사항입니다',
     like: 22,
     items: [
