f08fccd215c603e8457778350ebb684faf27a920
jinho1011 <gino9940@gmail.com>
2021-12-21 16:31:09 +0900

    Add displayType in recommands data

--- data/recommands/recommands.js
+++ data/recommands/recommands.js
@@ -3,12 +3,15 @@ export default {
     recomms: [
       {
         id: 'foodSets-1',
+        displayType: 'long',
       },
       {
         id: 'foodSingles-1',
+        displayType: 'short',
       },
       {
         id: 'foodSingles-2',
+        displayType: 'short',
       },
     ],
   },
@@ -16,12 +19,15 @@ export default {
     recomms: [
       {
         id: 'recSets-1',
+        displayType: 'long',
       },
       {
         id: 'recSingles-1',
+        displayType: 'short',
       },
       {
         id: 'recSingles-2',
+        displayType: 'short',
       },
     ],
   },
