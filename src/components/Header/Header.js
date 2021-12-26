7f71bb1918eb713dbc37da211633086c6fd9bab5
jinho1011 <gino9940@gmail.com>
2021-12-26 20:43:09 +0900

    Change Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,50 +3,80 @@ import {View, Text, Image} from 'react-native';
 import styled from 'styled-components';
 
 const HeaderContainer = styled.View`
-  flex: 1;
+  flex-direction: row;
   align-items: center;
-  justify-content: center;
-  background-color: #000;
+  justify-content: space-between;
+  background-color: #fff;
+  padding-left: 15px;
+  padding-right: 15px;
+  height: 58px;
+`;
+
+const HeaderBackButton = styled.Image`
+  width: 24px;
+  height: 24px;
+`;
+
+const HeaderTitleImage = styled.Image`
+  width: 127px;
 `;
 
 const HeaderProgram = styled.View`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 54px;
-  height: 54px;
-  margin-right: 10px;
+  margin-right: 14px;
 `;
 
 const HeaderProgramText = styled.Text`
-  color: #ffffff;
+  color: #373737;
   font-size: 10px;
 `;
 
+const HeaderProgramImage = styled.Image`
+  width: 24px;
+  height: 24px;
+`;
+
 const HeaderText = styled.Text`
-  color: #ffffff;
+  color: #373737;
   font-size: 18px;
   font-family: 'NotoSansKR-Bold';
 `;
 
+export const HeaderLeft = ({canGoBack}) => {
+  if (canGoBack) {
+    return <HeaderBackButton source={require('../../../assets/back_w.png')} />;
+  } else {
+    return <View style={{width: 24, height: 24}}></View>;
+  }
+};
+
+export const HeaderTitle = ({title}) => {
+  if (title == '홈') {
+    return <HeaderTitleImage source={require('../../../assets/mount.png')} />;
+  } else {
+    return <HeaderText>{title}</HeaderText>;
+  }
+};
+
 export const HeaderRight = () => {
   return (
-    // navigate to Program
     <HeaderProgram>
-      <Image source={require('../../../assets/projectfile_w.png')} />
+      <HeaderProgramImage
+        source={require('../../../assets/projectfile_w.png')}
+      />
       <HeaderProgramText>기획서</HeaderProgramText>
     </HeaderProgram>
   );
 };
 
-export const HeaderTitle = ({title}) => {
+export const Header = ({navigation, route, options, back}) => {
   return (
     <HeaderContainer>
-      {title == 'Home' ? (
-        <Image source={require('../../../assets/mount.png')} />
-      ) : (
-        <HeaderText>{title}</HeaderText>
-      )}
+      <HeaderLeft canGoBack={navigation.canGoBack()} />
+      <HeaderTitle title={options.title} />
+      <HeaderRight />
     </HeaderContainer>
   );
 };
