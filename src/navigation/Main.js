c76596fb4c9af082635086e8b4fbede2ef81a4d6
jinho1011 <gino9940@gmail.com>
2021-12-27 16:42:07 +0900

    Add Planner screen

--- src/navigation/Main.js
+++ src/navigation/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Tabs from './Tab';
 import Details from './Details';
+import Plan from '../screens/Main/Plan';
 
 const MainNavigator = createStackNavigator();
 
@@ -15,6 +16,11 @@ export default () => (
     <MainNavigator.Screen
       name="Details"
       component={Details}
+      options={{headerShown: false}}
+    />
+    <MainNavigator.Screen
+      name="Planner"
+      component={Plan}
       options={{headerShown: false}}
     />
   </MainNavigator.Navigator>
