4c18289fa743780002210b08a5383045376351fa
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-01-02 20:17:41 +0900

    수량 변경 수정중

--- src/screens/Main/Detail/FoodSet/FoodSetPresenter.js
+++ src/screens/Main/Detail/FoodSet/FoodSetPresenter.js
@@ -142,15 +142,9 @@ const FoodSetPresenter = ({state, setState}) => {
         <ChangeCountButton
           onPress={() => {
             const _state = state;
-            // let items = state.items.map(item => {
-            //   item.count = 0;
-            //   return item;
-            // });
-            console.log(_state.items[0].count);
             navigation.navigate('FoodSetChangeCount', {_state});
             setState(prev => ({
               ...prev,
-              // memberCnt: 0,
             }));
           }}>
           <ChangeCountText>수량변경</ChangeCountText>
