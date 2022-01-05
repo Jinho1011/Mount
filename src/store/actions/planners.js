2d59db59b4e65b3241954345f46655a18d23c2a5
jinho1011 <gino9940@gmail.com>
2022-01-05 20:52:06 +0900

    Update redux

--- src/store/actions/planners.js
+++ src/store/actions/planners.js
@@ -1,14 +1,14 @@
-export const ADD_PLANNER = 'ADD_PLANNER';
-export const DELETE_PLANNER = 'DELETE_PLANNER';
+export const CREATE_PLANNER = 'CREATE_PLANNER';
+export const ADD_ITEM = 'ADD_ITEM';
 export const MODIFY_TITLE = 'MODIFY_TITLE';
 export const UPDATE_PLANNER = 'UPDATE_PLANNER';
 
-export const addPlanner = planner => {
-  return {type: ADD_PLANNER, planner};
+export const createPlanner = title => {
+  return {type: CREATE_PLANNER, title};
 };
 
-export const deletePlanner = planner => {
-  return {type: DELETE_PLANNER, planner};
+export const addItem = (title, item) => {
+  return {type: ADD_ITEM, title, item};
 };
 
 export const modifyPlannerTitle = (planner, title) => {
