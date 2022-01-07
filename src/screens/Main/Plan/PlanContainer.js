ab7d610bc2a435022ab36c7fa7e67af5be1c4e96
jinho1011 <gino9940@gmail.com>
2022-01-07 21:22:46 +0900

    Init data by route params

--- src/screens/Main/Plan/PlanContainer.js
+++ src/screens/Main/Plan/PlanContainer.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useSelector} from 'react-redux';
 import PlanPresenter from './PlanPresenter';
 
-export default () => {
-  return <PlanPresenter />;
+export default ({navigation, route}) => {
+  const planners = useSelector(state => state.planners.planners);
+  const [state, setState] = useState({
+    plan: planners.filter(plan => plan.title === route.params.title),
+  });
+
+  useEffect(() => {}, []);
+
+  return <PlanPresenter state={state} setState={setState} />;
 };
