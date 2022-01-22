93a78a6e83761a631b42cbc6d916a1255803fa38
jinho1011 <gino9940@gmail.com>
2022-01-22 14:53:47 +0900

    Fix metro config

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,16 @@
-const {getDefaultCon  fig} = require('metro-config');
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const {getDefaultConfig} = require('metro-config');
 
-module.exports = (async() => {
+module.exports = (async () => {
   const {
-    resolver: { sourceExts, assetExts },
+    resolver: {sourceExts, assetExts},
   } = await getDefaultConfig();
-  return {  
+  return {
     transformer: {
       babelTransformerPath: require.resolve('react-native-svg-transformer'),
       getTransformOptions: async () => ({
         transform: {
           experimentalImportSupport: false,
-         inlineRequires: true,
+          inlineRequires: true,
         },
       }),
     },
@@ -24,6 +18,5 @@ module.exports = (async() => {
       assetExts: assetExts.filter(ext => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg'],
     },
-  }
-})
-
+  };
+})();
