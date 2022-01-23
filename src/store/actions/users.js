d153c8007c0530aaa94097b1495785c24bff6641
amykim928 <hyeonah928@gmail.com>
2022-01-24 00:30:30 +0900

    카카오 회원가입, 로그인 구현 중

--- src/store/actions/users.js
+++ src/store/actions/users.js
@@ -1,5 +1,22 @@
+export const KAKAO_SIGNUP = 'KAKAO_SIGNUP';
 export const LOGIN_USER = 'LOGIN_USER';
 export const SIGNUP_USER = 'SIGNUP_USER';
+
+export const kakaoSignup = dataToSubmit => {
+  let req;
+  if (
+    dataToSubmit.nickname === '김현아' &&
+    dataToSubmit.email === 'amykim928@naver.com'
+  ) {
+    req = true;
+  } else {
+    req = false;
+  }
+  return {
+    type: SIGNUP_USER,
+    payload: req, //reducer로
+  };
+};
 
 //서버에 리퀘스트 날리고 받은 데이터를 request에 저장
 export const signupUser = dataToSubmit => {
