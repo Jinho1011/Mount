014f6f5e6580b6f5030e6d24c63760ed8c0c420f
jinho1011 <gino9940@gmail.com>
2022-01-23 14:56:18 +0900

    Implement onPress methods

--- src/screens/Main/PlanEditor/PlanEditorPresenter.js
+++ src/screens/Main/PlanEditor/PlanEditorPresenter.js
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import {Dimensions, ScrollView, Pressable} from 'react-native';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import styled from 'styled-components';
 import {useNavigation} from '@react-navigation/native';
 import FocusAwareStatusBar from '../../../components/StatusBar';
+import {createPlanner, deletePlanner} from '../../../store/actions/planners';
 import {
   Planedit_newprojectfileSvg,
   MoreSvg,
@@ -36,6 +37,8 @@ const BtnText = styled.Text`
 
 const PlanersContainer = styled.View``;
 
+const PlannerContainer = styled.View``;
+
 const Planner = styled.Pressable`
   flex-direction: row;
   justify-content: space-between;
@@ -59,14 +62,22 @@ const Divider = styled.View`
 
 export default ({state, setState}) => {
   const [isEditing, setIsEditing] = useState(false);
+
+  const planners = useSelector(state => state.planners.planners);
   const navigation = useNavigation();
   const dispatch = useDispatch();
 
-  const navigateToPlan = id => {};
+  const navigateToPlan = id => {
+    navigation.navigate('Planner', {id});
+  };
 
-  const addNewPlan = () => {};
+  const addNewPlan = () => {
+    dispatch(createPlanner('새 기획서'));
+  };
 
-  const deletePlan = () => {};
+  const deletePlan = id => {
+    dispatch(deletePlanner(id));
+  };
 
   return (
     <PlanEditContainer>
@@ -83,22 +94,31 @@ export default ({state, setState}) => {
         </PlanBtn>
       </BtnContainer>
       <PlanersContainer>
-        <Planner>
-          <PlannerName>새 기획서</PlannerName>
-          {isEditing ? (
-            <Pressable onPress={deletePlan}>
-              <Planedit_minus_Svg width={24} height={24} />
-            </Pressable>
-          ) : (
-            <Pressable
-              onPress={() => {
-                navigateToPlan(0);
-              }}>
-              <MoreSvg width={24} height={24} />
-            </Pressable>
-          )}
-        </Planner>
-        <Divider />
+        {planners.map(p => {
+          return (
+            <PlannerContainer key={p.id}>
+              <Planner>
+                <PlannerName>{p.title}</PlannerName>
+                {isEditing ? (
+                  <Pressable
+                    onPress={() => {
+                      deletePlan(p.id);
+                    }}>
+                    <Planedit_minus_Svg width={24} height={24} />
+                  </Pressable>
+                ) : (
+                  <Pressable
+                    onPress={() => {
+                      navigateToPlan(p.id);
+                    }}>
+                    <MoreSvg width={24} height={24} />
+                  </Pressable>
+                )}
+              </Planner>
+              <Divider />
+            </PlannerContainer>
+          );
+        })}
       </PlanersContainer>
     </PlanEditContainer>
   );
