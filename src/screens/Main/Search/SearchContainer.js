52ac9f00dd0307adeae7c71000d5caff6f618cb6
jinho1011 <gino9940@gmail.com>
2022-01-24 22:37:58 +0900

    Replace mock data to search store

--- src/screens/Main/Search/SearchContainer.js
+++ src/screens/Main/Search/SearchContainer.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useSelector} from 'react-redux';
 import SearchPresenter from './SearchPresenter';
 
 export default () => {
-  return <SearchPresenter />;
+  const recents = useSelector(state => state.search);
+  const [state, setState] = useState({recents});
+  useEffect(() => {
+    console.log('🚀 ~ file: SearchContainer.js ~ line 11 ~ state', state);
+  }, [state]);
+  return <SearchPresenter state={state} setState={setState} />;
 };
