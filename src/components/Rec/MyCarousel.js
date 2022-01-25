1f2c5fa48f24e42bd0917b07a7c7adda7da38565
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-01-25 11:38:05 +0900

    캐로셀 구현

--- src/components/Rec/MyCarousel.js
+++ src/components/Rec/MyCarousel.js
@@ -9,37 +9,29 @@ import {
   Image,
 } from 'react-native';
 
-const ENTRIES1 = [
-  {
-    title: 'Beautiful and dramatic Antelope Canyon',
-    subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
-    illustration: 'https://i.imgur.com/UYiroysl.jpg',
-  },
-  {
-    title: 'Earlier this morning, NYC',
-    subtitle: 'Lorem ipsum dolor sit amet',
-    illustration: 'https://i.imgur.com/UPrs1EWl.jpg',
-  },
-  {
-    title: 'White Pocket Sunset',
-    subtitle: 'Lorem ipsum dolor sit amet et nuncat ',
-    illustration: 'https://i.imgur.com/MABUbpDl.jpg',
-  },
-  {
-    title: 'Acrocorinth, Greece',
-    subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
-    illustration: 'https://i.imgur.com/KZsmUi2l.jpg',
-  },
-  {
-    title: 'The lone tree, majestic landscape of New Zealand',
-    subtitle: 'Lorem ipsum dolor sit amet',
-    illustration: 'https://i.imgur.com/2nCt3Sbl.jpg',
-  },
-];
+// const ENTRIES1 = [
+//   {
+//     illustration: 'https://i.imgur.com/UYiroysl.jpg',
+//   },
+//   {
+//     illustration: 'https://i.imgur.com/UPrs1EWl.jpg',
+//   },
+//   {
+//     illustration: 'https://i.imgur.com/MABUbpDl.jpg',
+//   },
+//   {
+//     illustration: 'https://i.imgur.com/KZsmUi2l.jpg',
+//   },
+//   {
+//     illustration: 'https://i.imgur.com/2nCt3Sbl.jpg',
+//   },
+// ];
+
 const {width: screenWidth} = Dimensions.get('window');
 
 const MyCarousel = ({state, setState}) => {
-  // console.log(state?.recSingle?.guideImages);
+  const ENTRIES1 = state?.recSingle?.guideImages;
+  console.log(ENTRIES1);
   const [entries, setEntries] = useState([]);
   const carouselRef = useRef(null);
 
@@ -49,13 +41,13 @@ const MyCarousel = ({state, setState}) => {
 
   useEffect(() => {
     setEntries(ENTRIES1);
-  }, []);
+  }, [ENTRIES1]);
 
   const renderItem = ({item, index}, parallaxProps) => {
     return (
       <View style={styles.item}>
         <ParallaxImage
-          source={{uri: item.illustration}}
+          source={{uri: item}}
           containerStyle={styles.imageContainer}
           style={styles.image}
           parallaxFactor={0.4}
