e6f9ef8bc7b66c1ae40e61a6576b37496a020ab9
jinho1011 <gino9940@gmail.com>
2022-01-28 15:39:02 +0900

    Add Favorite screen

--- src/screens/Main/Favorite/FavoritePresenter.js
+++ src/screens/Main/Favorite/FavoritePresenter.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import styled from 'styled-components';
 import {View, Text} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
-export default () => {
+import Box from '../../../components/Main/Box';
+
+const FavContainer = styled.View`
+  padding: 24px 23px 95px 23px;
+`;
+
+const SmallBoxContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export default ({state}) => {
   const navigation = useNavigation();
+  const [itemArr, setItemArr] = useState([]);
+
+  useEffect(() => {
+    if (state.isReady) {
+      for (let i = 0; i < state.items.length; i++) {
+        let arr = [];
+        let item1 = state.items[i];
+        item1.displayType = 'short';
+        arr.push(item1);
+        if (i + 1 < state.items.length) {
+          let item2 = state.items[i + 1];
+          item2.displayType = 'short';
+          arr.push(item2);
+          i += 1;
+        }
+        setItemArr(prev => [...prev, arr]);
+      }
+    }
+  }, [state]);
+
   return (
-    <View>
-      <Text>Favorite</Text>
-    </View>
+    <FavContainer>
+      {itemArr.map(item => {
+        if (item.length === 2) {
+          return (
+            <SmallBoxContainer key={item[0].title + item[0].id}>
+              <Box item={item[0]} />
+              <Box item={item[1]} />
+            </SmallBoxContainer>
+          );
+        } else {
+          return (
+            <SmallBoxContainer key={item[0].title + item[0].id}>
+              <Box item={item[0]} />
+              <></>
+            </SmallBoxContainer>
+          );
+        }
+      })}
+    </FavContainer>
   );
 };
