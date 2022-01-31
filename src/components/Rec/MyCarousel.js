9939cdee26f58edafaea4706e0d6edad5767ca6e
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-01-31 22:16:21 +0900

    refactor: :fire: unused remove

--- src/components/Rec/MyCarousel.js
+++ src/components/Rec/MyCarousel.js
@@ -9,24 +9,6 @@ import {
   Image,
 } from 'react-native';
 
-// const ENTRIES1 = [
-//   {
-//     illustration: 'https://i.imgur.com/UYiroysl.jpg',
-//   },
-//   {
-//     illustration: 'https://i.imgur.com/UPrs1EWl.jpg',
-//   },
-//   {
-//     illustration: 'https://i.imgur.com/MABUbpDl.jpg',
-//   },
-//   {
-//     illustration: 'https://i.imgur.com/KZsmUi2l.jpg',
-//   },
-//   {
-//     illustration: 'https://i.imgur.com/2nCt3Sbl.jpg',
-//   },
-// ];
-
 const {width: screenWidth} = Dimensions.get('window');
 
 const MyCarousel = ({state, setState}) => {
