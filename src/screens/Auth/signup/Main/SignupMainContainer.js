2e85041ad238f4b51d96eec7c634750466e9f783
amykim928 <hyeonah928@gmail.com>
2022-02-04 05:56:30 +0900

    Add Google Login / Modify reducers

--- src/screens/Auth/signup/Main/SignupMainContainer.js
+++ src/screens/Auth/signup/Main/SignupMainContainer.js
@@ -1,9 +1,10 @@
-import React, {useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import React from 'react';
+import {useSelector} from 'react-redux';
 import SignupMainPresenter from './SignupMainPresenter';
 import {useNavigation} from '@react-navigation/native';
 import {login, getProfile} from '@react-native-seoul/kakao-login';
 //import {NaverLogin, getNaverProfile} from '@react-native-seoul/naver-login';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import {AsyncStorage} from 'react-native';
 
 export let KakaoBody = {
@@ -13,11 +14,17 @@ export let KakaoBody = {
   nickname: '',
 };
 
+export let GoogleBody = {
+  accessToken: '',
+  email: '',
+  name: '',
+};
+
 export default () => {
   const users = useSelector(state => state.users);
   const navigation = useNavigation();
   const signupDetailPress = () => navigation.navigate('SignupDetail');
-  const signInWithKakao = async () => {
+  const signupWithKakao = async () => {
     const token = await login();
     const profile = await getProfile();
 
@@ -37,10 +44,31 @@ export default () => {
       navigation.navigate('Tutorial');
     }
   };
+  const signupWithGoogle = async () => {
+    GoogleSignin.configure({});
+    const token = await GoogleSignin.getTokens();
+    const profile = await GoogleSignin.signIn();
+    if (token) {
+      GoogleBody = {
+        accessToken: token.accessToken,
+        email: profile.user.email,
+        name: profile.user.name,
+      };
+      AsyncStorage.setItem(
+        'userData',
+        JSON.stringify({
+          GoogleBody,
+        }),
+      );
+      navigation.navigate('Tutorial');
+    }
+  };
+
   return (
     <SignupMainPresenter
       signupDetailPress={signupDetailPress}
-      signInWithKakao={signInWithKakao}
+      signupWithKakao={signupWithKakao}
+      signupWithGoogle={signupWithGoogle}
     />
   );
 };
--- src/store/actions/users.js
+++ src/store/actions/users.js
@@ -1,13 +1,13 @@
-export const KAKAO_SIGNUP = 'KAKAO_SIGNUP';
+export const SOCIALMEDIA_SIGNUP = 'SOCIALMEDIA_SIGNUP';
 export const LOGIN_USER = 'LOGIN_USER';
 export const SIGNUP_USER = 'SIGNUP_USER';
 
-export const kakaoSignup = dataToSubmit => {
+export const socialMediaSignup = dataToSubmit => {
   // 서버에는 액세스토큰, Email, nickname 서버로 전송
   // jwt = asdaskjfhasjkjkasld
   let req = true;
   return {
-    type: KAKAO_SIGNUP,
+    type: SOCIALMEDIA_SIGNUP,
     payload: req, //reducer로
   };
 };
