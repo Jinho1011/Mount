be47eff2b26f8b1536b34f5edab74937d1ca7a36
amykim928 <hyeonah928@gmail.com>
2022-02-04 07:29:13 +0900

    Add Google Login / Modify reducers

--- src/screens/Auth/tutorial/tutorialContainer.js
+++ src/screens/Auth/tutorial/tutorialContainer.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import TutorialPresenter from './tutorialPresenter';
-import {kakaoSignup, signupUser, loginUser} from '../../../store/actions/users';
-import {KakaoBody} from '../signup/Main/SignupMainContainer';
+import {
+  socialMediaSignup,
+  signupUser,
+  loginUser,
+} from '../../../store/actions/users';
+import {KakaoBody, GoogleBody} from '../signup/Main/SignupMainContainer';
 import {EtcSignBody} from '../signup/Detail/SignupDetailContainer';
 import {EtcLoginBody} from '../login/Detail/LoginDetailContainer';
 import {useDispatch} from 'react-redux';
@@ -10,13 +14,14 @@ export default () => {
   const dispatch = useDispatch();
   const startPress = () => {
     if (KakaoBody.accessToken !== '') {
-      dispatch(kakaoSignup(KakaoBody));
+      dispatch(socialMediaSignup(KakaoBody));
+    } else if (GoogleBody.accessToken !== '') {
+      dispatch(socialMediaSignup(GoogleBody));
     } else if (EtcSignBody.email !== '') {
       dispatch(signupUser(EtcSignBody));
     } else if (EtcLoginBody.email !== '') {
       dispatch(loginUser(EtcLoginBody));
     }
   };
-
   return <TutorialPresenter startPress={startPress} />;
 };
