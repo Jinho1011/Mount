2c62807028fdf51d2950b9ff19c93ad7291e0acc
jinho1011 <gino9940@gmail.com>
2022-02-05 01:30:20 +0900

    Add save as xlsx

--- android/app/src/main/java/com/mount/MainActivity.java
+++ android/app/src/main/java/com/mount/MainActivity.java
@@ -1,4 +1,6 @@
 package com.mount;
+import expo.modules.ReactActivityDelegateWrapper;
+import com.facebook.react.ReactActivityDelegate;
 import com.reactnativekakaosharelink.KakaoShareLinkPackage;
 import com.facebook.react.ReactActivity;
 import com.rnfs.RNFSPackage; // <------- add package
@@ -12,5 +14,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Mount";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegateWrapper(this,
+      new ReactActivityDelegate(this, getMainComponentName())
+    );
   }
 }
