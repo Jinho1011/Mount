26cb13b277cdb6b54ea31361b288f6a43a887076
jinho1011 <gino9940@gmail.com>
2022-02-05 20:11:47 +0900

    Add api

--- src/screens/Main/Home/Main/HomeMainContainer.js
+++ src/screens/Main/Home/Main/HomeMainContainer.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
+import {getDatas} from '../../../../api/api';
 
 import HomePresenter from './HomeMainPresenter';
 import * as recommandsActions from '../../../../store/actions/recommands';
@@ -13,43 +14,20 @@ export default () => {
   });
 
   const _loadData = async () => {
-    const data = await fetch('/api/recommands');
-    const _foods = JSON.parse(data._bodyInit).foods.recomms;
-    const _recs = JSON.parse(data._bodyInit).recs.recomms;
-
-    const foods = await _loadItems(_foods, 'foods');
-    const recs = await _loadItems(_recs, 'recs');
+    const foods = await getDatas('foods');
+    const recs = await getDatas('recs');
 
     setState(prev => ({
       ...prev,
       foods,
       recs,
+      isLoaded: true,
     }));
-
-    _setLoaded();
-  };
-
-  const _loadItems = async items => {
-    return await Promise.all(
-      items.map(async function (_item) {
-        const id = _item.id.split('-');
-        const displayType = _item.displayType;
-        const type = id[0].slice(0, -1);
-        const data = await fetch(`/api/${id[0]}/${id[1]}`);
-        const item = JSON.parse(data._bodyInit)[type];
-        item['type'] = type;
-        item['displayType'] = displayType;
-        return item;
-      }),
-    );
-  };
-
-  const _setLoaded = () => {
-    setState(prev => ({...prev, isLoaded: true}));
   };
 
   useEffect(() => {
     const init = async () => {
+      //
       _loadData();
     };
     init();
@@ -57,8 +35,16 @@ export default () => {
 
   useEffect(() => {
     if (state.foods.length > 0 && state.recs.length > 0) {
-      dispatch(recommandsActions.initFoods(state.foods));
-      dispatch(recommandsActions.initRecs(state.recs));
+      console.log(
+        '🚀 ~ file: HomeMainContainer.js ~ line 37 ~ useEffect ~ state',
+        state.foods[0],
+      );
+      console.log(
+        '🚀 ~ file: HomeMainContainer.js ~ line 37 ~ useEffect ~ state',
+        state.recs[0],
+      );
+      // dispatch(recommandsActions.initFoods(state.foods));
+      // dispatch(recommandsActions.initRecs(state.recs));
     }
   }, [state]);
 
