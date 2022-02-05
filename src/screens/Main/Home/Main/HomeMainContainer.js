7eda8a1e667d0809fbb261adcdff61ac38091802
jinho1011 <gino9940@gmail.com>
2022-02-05 21:03:28 +0900

    Add dp type

--- src/screens/Main/Home/Main/HomeMainContainer.js
+++ src/screens/Main/Home/Main/HomeMainContainer.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import {getDatas} from '../../../../api/api';
+import {getFoodSets, getRecSets} from '../../../../api/api';
 
 import HomePresenter from './HomeMainPresenter';
 import * as recommandsActions from '../../../../store/actions/recommands';
@@ -13,36 +13,35 @@ export default () => {
     isLoaded: false,
   });
 
-  const _loadData = async () => {
-    const foods = await getDatas('foods');
-    const recs = await getDatas('recs');
+  const loadData = async () => {
+    const foodSets = await getFoodSets();
+    const recSets = await getRecSets();
 
-    setState(prev => ({
-      ...prev,
-      foods,
-      recs,
-      isLoaded: true,
-    }));
+    for (let i = 0, max = foodSets.length; i < max; i++) {
+      if (Math.random() > 0.5) {
+        foodSets[i].display = 'long';
+        if (i + 1 < foodSets.length) foodSets[i + 1].display = 'long';
+      } else {
+      }
+    }
+
+    // setState(prev => ({
+    //   ...prev,
+    //   foods,
+    //   recs,
+    //   isLoaded: true,
+    // }));
   };
 
   useEffect(() => {
     const init = async () => {
-      //
-      _loadData();
+      loadData();
     };
     init();
   }, []);
 
   useEffect(() => {
     if (state.foods.length > 0 && state.recs.length > 0) {
-      console.log(
-        '🚀 ~ file: HomeMainContainer.js ~ line 37 ~ useEffect ~ state',
-        state.foods[0],
-      );
-      console.log(
-        '🚀 ~ file: HomeMainContainer.js ~ line 37 ~ useEffect ~ state',
-        state.recs[0],
-      );
       // dispatch(recommandsActions.initFoods(state.foods));
       // dispatch(recommandsActions.initRecs(state.recs));
     }
