ba39ceb3229f4586b1abedfdaf4eda5b9dbe114e
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-02-06 16:57:58 +0900

    fix: :fire: remove unused

--- src/screens/Main/FoodPlan/FoodPlanPresenter.js
+++ src/screens/Main/FoodPlan/FoodPlanPresenter.js
@@ -1,36 +1,36 @@
 import {useNavigation} from '@react-navigation/native';
-import React, {useState, useEffect} from 'react';
-import {View, Text, Pressable} from 'react-native';
+import React from 'react';
+import {View, Text, Pressable, ScrollView, Image} from 'react-native';
 import styled from 'styled-components';
 import Item from '../../../components/Food/Item';
 
-const ScrollContainer = styled.ScrollView`
+const ScrollContainer = styled(ScrollView)`
   background-color: #fff;
   padding: 18px 20px 0 20px;
 `;
 
-const CountBox = styled.View`
+const CountBox = styled(View)`
   background-color: #e2f955;
   border-radius: 12px;
   padding: 25px 30px 25px 30px;
   align-items: center;
 `;
 
-const CountBoxTitle = styled.Text`
+const CountBoxTitle = styled(Text)`
   font-family: 'NotoSansKR-Bold';
   font-size: 18px;
   line-height: 26px;
   color: #000000;
 `;
 
-const Counter = styled.View`
+const Counter = styled(View)`
   flex-direction: row;
   justify-content: space-between;
   padding-top: 20px;
   margin: auto;
 `;
 
-const MinusButton = styled.Pressable`
+const MinusButton = styled(Pressable)`
   background: #b4b4b4;
   opacity: 0.6;
   border-radius: 16px;
@@ -40,65 +40,65 @@ const MinusButton = styled.Pressable`
   margin-right: 20px;
 `;
 
-const Minus = styled.Image`
+const Minus = styled(Image)`
   margin: auto;
 `;
 
-const PlusButton = styled.Pressable`
+const PlusButton = styled(Pressable)`
   background: #373737;
   border-radius: 16px;
   margin-left: 20px;
   padding: 7px;
 `;
 
-const Plus = styled.Image`
+const Plus = styled(Image)`
   margin: auto;
 `;
 
-const NumberBox = styled.View`
+const NumberBox = styled(View)`
   padding-left: 45px;
   padding-right: 45px;
   background: #ffffff;
   border-radius: 5px;
 `;
 
-const Number = styled.Text`
+const Number = styled(Text)`
   font-family: 'NotoSansKR-Bold';
   font-size: 16px;
   line-height: 24px;
 `;
 
-const FoodsContainer = styled.View`
+const FoodsContainer = styled(View)`
   padding-top: 26px;
 `;
 
-const TopView = styled.View`
+const TopView = styled(View)`
   flex-direction: row;
   justify-content: space-between;
 `;
 
-const MountLogoImage = styled.Image`
+const MountLogoImage = styled(Image)`
   margin: auto;
 `;
 
-const TitleBox = styled.View`
+const TitleBox = styled(View)`
   justify-content: center;
 `;
 
-const Footer = styled.View`
+const Footer = styled(View)`
   padding: 8px 23px;
   background: #ffffff;
   border: 0.3px solid #b4b4b4;
 `;
 
-const ChangeCountButton = styled.Pressable`
+const ChangeCountButton = styled(Pressable)`
   padding: 12px 128px;
-  background: ${prop => (prop.pressedCnt > 0 ? '#e2f955' : '#f3f3f3')} 
+  background: ${prop => (prop.pressedCnt > 0 ? '#e2f955' : '#f3f3f3')};
   border-radius: 5px;
 `;
 
-const ChangeCountButtonText = styled.Text`
-  font-family: 'NotoSansKR-Regular'
+const ChangeCountButtonText = styled(Text)`
+  font-family: 'NotoSansKR-Regular';
   font-size: 16px;
   line-height: 24px;
   margin: auto;
